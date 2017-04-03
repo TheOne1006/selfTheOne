a925893369a629d995b7d159fa1cb42bca288a97
theone <297190869@qq.com>
2017-04-03 16:16:19 +0800

    init

---
+++ app/index.js
@@ -0,0 +1,143 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'fullpage.js/dist/jquery.fullpage.min.css';
+import 'google-code-prettify/bin/prettify.min';
+import 'fullpage.js/dist/jquery.fullpage.min';
+import Vivus from 'vivus/dist/vivus';
+
+import '../assets/styles/index.scss';
+// ================================
+// START YOUR APP HERE
+// ================================
+$(window.document).ready(() => {
+  const animateHooks = {
+    // 展示
+    bottom: 'moveUp',
+    top: 'moveDown',
+    left: 'moveRight',
+    right: 'moveLeft',
+    topLeft: 'moveDownRight',
+    bottomLeft: 'moveUpRight',
+    topRight: 'moveDownLeft',
+    bottomRight: 'moveUpLeft',
+  };
+
+  // 是否为移动设备
+  const isMobile = window.navigator.userAgent.match(/mobile/i);
+  // 缓存图片数据
+  const svgImgs = {
+    sectionAll: $('#sectionAll .svgimg'),
+    sectionInfo: $('#sectionInfo .svgimg'),
+    welcomeTop: $('.welcome-top'),
+    welcomeBottom: $('.welcome-bottom'),
+  };
+
+  // 初始化
+  svgImgs.sectionAll.addClass(function addClass() {
+    return animateHooks[$(this).data('animate')];
+  });
+
+  window.prettyPrint();
+
+  const vivusTheOne = new Vivus('vivusTheOne', {
+    type: 'scenario-sync',
+    duration: 10,
+    start: 'autostart',
+    dashGap: 10,
+    forceRender: false,
+  });
+  $('#vivusTheOne').click(() => {
+    vivusTheOne.reset().play();
+  });
+
+  let vivusTree;
+  function treePlay() {
+    if (!vivusTree) {
+      vivusTree = new Vivus('tree', {
+        type: 'scenario',
+        file: '/assets/svgs/tree/tree.svg',
+      });
+
+      // hack 加载进度
+      setTimeout(() => {
+        vivusTree.reset().play();
+      }, 2000);
+    } else {
+      vivusTree.reset().play();
+    }
+  }
+
+  // fullpage
+  $('#fullpage').fullpage({
+    // 在复杂情况下还是会失效
+    // sectionsColor: ['#563d7c' ,'#5bc0de','#5cb85c','#f0ad4e','#d9534f'],
+    anchors: ['all', 'page2', 'page3', 'page4'],
+    // 移动设备启用css3
+    css3: isMobile,
+    controlArrows: !isMobile,
+    verticalCentered: false,
+    animateAnchor: true,
+    recordHistory: true,
+    afterRender: () => {
+      // 渲染完成
+      setTimeout(() => {
+        svgImgs.sectionAll.removeClass(function removeClass() {
+          return animateHooks[$(this).data('animate')];
+        });
+      }, 500);
+    },
+    onLeave: (index, nextIndex) => {
+      switch (index) {
+        case 1:
+          $('#staticLogo').addClass('active');
+
+          svgImgs.sectionAll.addClass(function addClass() {
+            return animateHooks[$(this).data('animate')];
+          });
+          break;
+        case 2:
+          if (!isMobile) {
+            svgImgs.sectionInfo.removeClass('flashAndtada');
+          }
+          break;
+        case 3:
+          svgImgs.welcomeBottom.removeClass('welcome-animate-bottom');
+          svgImgs.welcomeTop.removeClass('welcome-animate-top');
+          break;
+        default:
+          // treePlay('rewind');
+          break;
+
+      }
+
+      switch (nextIndex) {
+        case 1:
+          $('#staticLogo').removeClass('active');
+          svgImgs.sectionAll.removeClass(function removeClass() {
+            return animateHooks[$(this).data('animate')];
+          });
+          break;
+        case 2:
+          if (!isMobile) {
+            svgImgs.sectionInfo.addClass('flashAndtada');
+          }
+          break;
+        case 3:
+          setTimeout(() => {
+            svgImgs.welcomeBottom.addClass('welcome-animate-bottom');
+            svgImgs.welcomeTop.addClass('welcome-animate-top');
+          }, 800);
+          break;
+        case 4:
+          $('#tree').show();
+          treePlay();
+          break;
+        default:
+          break;
+      }
+    },
+  });
+});
---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const dirApp = path.join(__dirname, 'app');
+const dirAssets = path.join(__dirname, 'assets');
+
+// Is the current build a development build
+const IS_DEV = (process.env.NODE_ENV === 'dev');
+
+module.exports = {
+  entry: {
+    vendor: [
+      'jquery',
+      'lodash',
+    ],
+    bundle: path.join(dirApp, 'index'),
+  },
+  resolve: {
+    modules: [
+      'node_modules',
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEV,
+    }),
+
+    new webpack.ProvidePlugin({
+      // jQuery
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'root.jQuery': 'jquery',
+      // lodash
+      '_': 'lodash',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.ejs'),
+      title: 'TheOne.io 个人简介',
+    }),
+  ],
+  module: {
+    rules: [
+      // BABEL
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        options: {
+          compact: true,
+        },
+      },
+      // STYLES
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: IS_DEV,
+            },
+          },
+        ],
+      },
+
+      // CSS / SASS
+      {
+        test: /\.scss/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: IS_DEV,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: IS_DEV,
+              includePaths: [dirAssets],
+            },
+          },
+        ],
+      },
+
+      // EJS
+      {
+        test: /\.ejs$/,
+        loader: 'ejs-loader',
+      },
+
+      // IMAGES
+      {
+        test: /\.(jpe*g|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.woff(\?.*)?$/,
+        loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.woff2(\?.*)?$/,
+        loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/font-woff2',
+      },
+      {
+        test: /\.otf(\?.*)?$/,
+        loader: 'file-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=font/opentype' },
+      {
+        test: /\.ttf(\?.*)?$/,
+        loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?.*)?$/,
+        loader: 'file-loader?prefix=fonts/&name=[path][name].[ext]',
+      },
+      {
+        test: /\.svg(\?.*)?$/,
+        loader: 'url-loader?prefix=fonts/&name=[path][name].[ext]&limit=10000&mimetype=image/svg+xml',
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url?limit=8192',
+      },
+    ],
+  },
+};
